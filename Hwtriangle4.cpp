8089c1f7cade62da72d084a117cab8474239cfd2
Ujjawal Singh <ujjawalkumar952388@gmail.com>
2026-05-19 00:02:27 +0530

    All HW is completed

---
+++ Hwtriangle4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'E' - i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+}
